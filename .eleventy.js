4d1fffe1d127ed1c1f4cff568e836b1e97914c34
Ivan Frantar <ivan.frantar@stelltec.com>
2021-01-05 22:43:04 +0000

    Rejigged the file structure of the site with something simpler.

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,18 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images"); // this is so 11ty don't process this folder when dumping it on _site
   eleventyConfig.addPassthroughCopy("css");
+
+  
+  
+  
+  return {
+    markdownTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    dir: {
+      input: 'src',
+      output: 'public'
+    }
+  };
+
 };
